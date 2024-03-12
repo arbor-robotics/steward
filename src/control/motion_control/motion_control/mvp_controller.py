8fef448a42ec230b33bc5ef43d7053171d393432
Will Heitman <will@heit.mn>
2024-03-12 14:11:14 -0400

    Clean up MVP controller

--- src/control/motion_control/motion_control/mvp_controller.py
+++ src/control/motion_control/motion_control/mvp_controller.py
@@ -24,7 +24,8 @@ class RoutePlanner(Node):
 
         self.route = []
 
-        self.create_subscription(Path, "/planning/trajectory", self.pathCb, 10)
+        # Note that the trajectory is not used in this minimal controller
+        self.create_subscription(Path, "/planning/trajectory", self.trajectoryCb, 10)
         self.create_subscription(Route, "/planning/full_route", self.routeCb, 10)
 
         self.tf_buffer = Buffer()
@@ -70,16 +71,11 @@ class RoutePlanner(Node):
         ego_y = base_link_to_map_tf.transform.translation.y
         q = base_link_to_map_tf.transform.rotation
 
-        # This is a very rough approximation of yaw
-        # TODO: Use scipy rotations
+        # Use scipy to calculate ego yaw
         orientation_euler = R.from_quat([q.x, q.y, q.z, q.w]).as_euler("xyz")
-        ego_yaw = orientation_euler[2] + np.pi / 2
 
-        # # Lock yaw into [-pi, pi]
-        # if ego_yaw > math.pi:
-        #     ego_yaw -= 2 * math.pi
-        # if ego_yaw < -math.pi:
-        #     ego_yaw += 2 * math.pi
+        # Add pi/2 to make "zero" point east (ENU) instead of north
+        ego_yaw = orientation_euler[2] + np.pi / 2
 
         # Get ego distance from next route point
         next_route_pt = self.route[0]
@@ -99,59 +95,37 @@ class RoutePlanner(Node):
         if dist < WAYPOINT_REACHED_THRESHOLD:
             self.route.pop(0)
 
-        # self.get_logger().info(
-        #     f"Targ: {next_route_pt.astype(int)}. Ego: {int(ego_x)}, {int(ego_y)}, yawerr {yaw_error}"
-        # )
-
-        # target_heading = math.atan2(dy, dx)
-        # # Lock target_heading into [-pi, pi]
-        # if target_heading > math.pi:
-        #     target_heading -= 2 * math.pi
-        # if target_heading < -math.pi:
-        #     target_heading += 2 * math.pi
-
-        # # yaw_error = target_heading - ego_yaw
         if yaw_error < -math.pi:
             yaw_error += 2 * math.pi
         if yaw_error > math.pi:
             yaw_error -= 2 * math.pi
-        # self.get_logger().info(
-        #     f"We're {dist} m away, heading {math.degrees(target_heading)}"
-        # )
-        # self.get_logger().info(
-        #     f"Heading is {int(math.degrees(ego_yaw))}, targ {int(x), int(y)}, err {int(math.degrees(yaw_error))}, dist {int(dist)}"
-        # )
 
         command_msg = Twist()
 
         angular_twist_val = 8.0
 
-        # if abs(yaw_error) > math.radians(30)
-
         if yaw_error < math.radians(-10):
-            self.get_logger().info("TURN RIGHT")
+            self.get_logger().debug("TURN RIGHT")
             command_msg.linear.x = 0.5
         elif yaw_error > math.radians(10):
-            self.get_logger().info("TURN LEFT")
+            self.get_logger().debug("TURN LEFT")
             command_msg.angular.z = -angular_twist_val
             command_msg.linear.x = 0.5
         else:
             command_msg.angular.z = 0.0
             command_msg.linear.x = 1.0
-            self.get_logger().info("DRIVE STRAIGHT")
+            self.get_logger().debug("DRIVE STRAIGHT")
 
         command_msg.angular.z = yaw_error
         self.twistPub.publish(command_msg)
 
-    def pathCb(self, msg: Path):
-        self.get_logger().info("Got a path!")
+    def trajectoryCb(self, msg: Path):
+        """Not used in this node.
 
-    def publishSampleTwist(self):
-        twist_msg = Twist()
-        # twist_msg.linear.x = 2.0  # m/s
-        twist_msg.angular.z = 1.0  # rad/s
-
-        self.twistPub.publish(twist_msg)
+        Args:
+            msg (Path): Trajectory message
+        """
+        self.get_logger().info("Got a trajectory!")
 
     def getHeader(self) -> Header:
         msg = Header()
@@ -160,10 +134,7 @@ class RoutePlanner(Node):
         return msg
 
     def setUpParameters(self):
-        # bounds_map_path_param_desc = ParameterDescriptor()
-        # bounds_map_path_param_desc.description = "File path to planting bounds image"
-        # bounds_map_path_param_desc.type = ParameterType.PARAMETER_STRING
-        # self.declare_parameter("bounds_map_path", True, bounds_map_path_param_desc)
+        """Not used."""
         pass
 
 
