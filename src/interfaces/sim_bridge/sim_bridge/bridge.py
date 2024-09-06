3ef23e73fbfa09629ea3fb4b7fd42edda0a728a5
Will Heitman <16737488+wheitman@users.noreply.github.com>
2024-09-05 20:11:54 -0400

    Add minimal Greet gRPC server

--- src/interfaces/sim_bridge/sim_bridge/bridge.py
+++ src/interfaces/sim_bridge/sim_bridge/bridge.py
@@ -3,15 +3,30 @@
 import asyncio
 from websockets.asyncio.server import serve
 
+import importlib.util
+import sys
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+from sim_bridge.protos.std_msgs import helloworld_pb2, helloworld_pb2_grpc
 
-
-async def main():
-    async with serve(echo, "localhost", 8787, max_size=2**21):
-        await asyncio.get_running_loop().create_future()  # run forever
+import grpc
+from concurrent import futures
 
 
-asyncio.run(main())
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
+
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+def main():
+    # logging.basicConfig()
+    serve()
