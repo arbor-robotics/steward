fda98be122f4b9a5af6ee2ed73c573763e79ee5a
Will Heitman <16737488+wheitman@users.noreply.github.com>
2024-09-08 20:05:00 -0400

    Stream images at about 50 Hz

--- src/interfaces/sim_bridge/sim_bridge/bridge.py
+++ src/interfaces/sim_bridge/sim_bridge/bridge.py
@@ -8,11 +8,16 @@ import rclpy
 from rclpy.node import Node, ParameterDescriptor, ParameterType
 from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile, ReliabilityPolicy
 from sensor_msgs.msg import Image
+import numpy as np
+from cv_bridge import CvBridge
+from PIL import Image as PILImage
 
 
 class WebsocketBridge(Node):
     def __init__(self):
         super().__init__("websocket_bridge")
+
+        self.bridge = CvBridge()
 
         self.setUpParameters()
 
@@ -29,47 +34,73 @@ class WebsocketBridge(Node):
         # asyncio.create_task(self.runServer)
 
     def publishHeartbeat(self):
-        self.get_logger().info("I'm still alive!")
+        # self.get_logger().info("I'm still alive!")
+        pass
 
     async def handleConnection(self, websocket):
         async for message in websocket:
             await websocket.send(message)
+
+    def publishImageBytes(self, bytes):
+        bytes_arr = np.array(PILImage.open(io.BytesIO(bytes)))
+
+        msg = self.bridge.cv2_to_imgmsg(bytes_arr, "rgba8")
+
+        # bytes_arr = mpimg.imread()
+        # plt.imshow(bytes_arr)
+        # self.get_logger().info(str(bytes_arr))
+        # msg: Image = self.numpy_to_image(bytes_arr)
+        self.imagePub.publish(msg)
+        # plt.show()
+
+    def numpy_to_image(self, arr, encoding="rgba8"):
+        # if not encoding in name_to_dtypes:
+        #     raise TypeError('Unrecognized encoding {}'.format(encoding))
+
+        im = Image(encoding=encoding)
+
+        # extract width, height, and channels
+        # dtype_class, exp_channels = name_to_dtypes[encoding]
+        dtype_class, exp_channels = (np.uint8, 4)
+        dtype = np.dtype(dtype_class)
+        if len(arr.shape) == 2:
+            im.height, im.width, channels = arr.shape + (1,)
+        elif len(arr.shape) == 3:
+            im.height, im.width, channels = arr.shape
+        else:
+            raise TypeError("Array must be two or three dimensional")
+
+        # check type and channels
+        if exp_channels != channels:
+            raise TypeError(
+                "Array has {} channels, {} requires {}".format(
+                    channels, encoding, exp_channels
+                )
+            )
+        if dtype_class != arr.dtype.type:
+            raise TypeError(
+                "Array is {}, {} requires {}".format(
+                    arr.dtype.type, encoding, dtype_class
+                )
+            )
+
+        # make the array contiguous in memory, as mostly required by the format
+        contig = np.ascontiguousarray(arr)
+        im.data = contig.tostring()
+        im.step = contig.strides[0]
+        im.is_bigendian = (
+            arr.dtype.byteorder == ">"
+            or arr.dtype.byteorder == "="
+            and sys.byteorder == "big"
+        )
+
+        return im
 
     # async def runServer(self):
 
     def setUpParameters(self):
         """Not used."""
         pass
-
-
-# def main(args=None):
-#     rclpy.init(args=args)
-
-#     node = WebsocketBridge()
-
-#     async def runServer():
-#         async with serve(node.handleConnection, "localhost", 8765):
-#             await asyncio.get_running_loop().create_future()  # run forever
-
-#     async def spinNode():
-#         rclpy.spin_once(node)
-
-#     async def run():
-#         return await asyncio.gather(runServer(), spinNode())
-
-#     rclpy.spin_once(node)
-
-#     rclpy.get_global_executor().create_task(runServer)
-
-#     # asyncio.run(run())
-
-#     rclpy.spin(node)
-
-#     # Destroy the node explicitly
-#     # (optional - otherwise it will be done automatically
-#     # when the garbage collector destroys the node object)
-#     node.destroy_node()
-#     rclpy.shutdown()
 
 
 import asyncio
@@ -84,6 +115,7 @@ node = WebsocketBridge()
 
 async def echo(websocket):
     async for message in websocket:
+        node.publishImageBytes(message)
         await websocket.send(message)
 
 
@@ -92,22 +124,12 @@ async def serverMain():
         await asyncio.get_running_loop().create_future()  # run forever
 
 
-async def start():
-    print("Node started.")
-
-    async def msg_callback(msg):
-        print(f"Message received: {msg}")
-
-    node.create_subscription(String, "/test", msg_callback, 10)
-    print("Listening to topic /test...")
-
-
 async def ros_loop():
     while rclpy.ok():
         rclpy.spin_once(node, timeout_sec=0)
-        await asyncio.sleep(1e-4)
+        await asyncio.sleep(1e-5)
 
 
 def main():
-    future = asyncio.wait([ros_loop(), start(), serverMain()])
+    future = asyncio.wait([ros_loop(), serverMain()])
     asyncio.get_event_loop().run_until_complete(future)
